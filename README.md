db9613de2e95c25d3584eac09d189ffae462e94b
achtor <peterx@uchicago.edu>
2014-12-23 23:41:38 -0600

    Initial commit, with basic edgarquery functionality.

---
+++ README.md
@@ -0,0 +1,13 @@
+# seek-degas
+
+This is a simple library for scraping the SEC's EDGAR resource. Companies like seekinf charge for full access to the data, despite it being free on their FTP server.
+
+Feel free to contribute; there is quite a bit of functionality that is still missing. Some of it is because I don't have enough experience with the data to know what's going on with it.
+
+Immediate TODO list:
+- Right now, the <pre>sic</pre> argument in <pre>edgarquery()</pre> doesn't do anything. Use the data, gleaned from the results of <a href="https://github.com/mattkiefer/sec-sic-scraper/blob/master/scraper.py">Matt Kiefer's scraper</a>, to improve this.
+- Implement XBRL search functionality - not actually sure what this is yet.
+- Keyphrase search is incredibly slow right now due to the necessity of opening and searching each individual text file from the FTP server, but the commercial resource mentioned above shows that it doesn't have to be slow - figure out how to change this. 
+- Make availability of the output in other formats part of the library - downloading a CSV, etc. Downloading the archival files made available by the SEC may also be useful.
+- Allow filtering by finer timeframes than entire years. Quarters at the least; are individual days necessary/useful?
+- Create a web interface for easy use.
---
+++ edgarquery.py
@@ -0,0 +1,59 @@
+import urllib2
+from datetime import date
+from dateutil.parser import parse
+
+"""
+Function edgarquery takes in:
+ - start, a numerical year since 1993. Defaults to 1993.
+ - end, a numerical year between start and the present year.
+ - form, a list of strings of form types to search for. (See list of form-types) Defaults
+   to None, in which case all forms are searched for.
+ - company, the desired company exact name (no punctuation, case-insensitive), as a string.
+   Defaults to none, in which case all companies are searched for.
+ - cik, the desired CIK number as a string. Defaults to None, in which case all CIK numbers 
+   are searched for.
+ - sic, the desired SIC number as a string. Defaults to None, in which case all SIC numbers
+   are searched for.
+ - keyphrases, a list of strings of exact phrases to search for
+"""
+def edgarquery(start = 1993, end = int(date.today().year), forms = None, company = None,
+               cik = None, sic = None, keyphrases = None):
+   # list of results
+   results = []
+
+   # Cycle through all the FTP holdings between the two dates
+   for year in range(start, end + 1):
+      for q in range(1,5):
+         try:
+            f = urllib2.urlopen('ftp://ftp.sec.gov/edgar/full-index/' + str(year) + '/QTR' 
+                                 + str(q) + '/master.idx')
+         except Exception,e:
+            print str(e)
+            return # Out of range? Move on
+         lines = f.read().splitlines()[10:]
+         for line in lines:
+            fdata = line.split('|')
+            
+            # Apply the tests from the arguments, storing each in a separate boolean
+            cik_inc = fdata[0] == cik or cik is None
+            company_inc = company is None or fdata[1].lower() == company.lower()
+            form_inc = forms is None or fdata[2] in forms
+
+            if cik_inc and company_inc and form_inc:
+               # if there is a keyphrase search, do that now
+               if keyphrases is not None:
+                  try:
+                     doc = urllib2.urlopen('ftp://ftp.sec.gov/' + fdata[4])
+                     flag = False
+                     for phrase in keyphrases:
+                        if phrase.lower() in doc.read().lower():
+                           flag = True
+                     if not flag:
+                        continue
+                  except:
+                     continue
+                     # do nothing and just keep going; some files are just missing.
+               results.append({'cik': fdata[0], 'company': fdata[1], 'form': fdata[2],
+                               'date': fdata[3], 'url': 'ftp://ftp.sec.gov/' + fdata[4]})
+               print str(results[-1])
+
