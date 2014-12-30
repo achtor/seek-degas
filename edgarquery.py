78352dd6c4ae5ec1b338769d27c3d6ac3f6fffcb
achtor <peterx@uchicago.edu>
2014-12-29 18:01:26 -0600

    decorator version of seekdegas

--- edgarquery.py
+++ edgarquery.py
@@ -14,49 +14,52 @@ Function query takes in:
    are searched for.
  - keyphrases, a list of strings of exact phrases to search for
 """
-def query(start = 1993, end = int(date.today().year), forms = None, company = None,
+def edgar(func):
+   def inner(start = 1993, end = int(date.today().year), forms = None, company = None,
                cik = None, sic = None, keyphrases = None):
-   # list of results
-   results = []
+      # list of results
+      results = []
 
-   # Cycle through all the FTP holdings between the two dates
-   for year in range(start, end + 1):
-      for q in range(1,5):
-         print 'Year ' + str(year) + ' quarter ' + str(q)
-         try:
-            f = urllib2.urlopen('ftp://ftp.sec.gov/edgar/full-index/' + str(year) + '/QTR' 
-                                 + str(q) + '/master.idx')
-         except Exception,e:
-            print str(e)
-            return # Out of range? Move on
-         lines = f.read().splitlines()[10:]
-         for line in lines:
-            fdata = line.split('|')
-            
-            # Apply the tests from the arguments, storing each in a separate boolean
-            cik_inc = fdata[0] == cik or cik is None
-            company_inc = company is None or fdata[1].lower() == company.lower()
-            form_inc = forms is None or fdata[2] in forms
+      # Cycle through all the FTP holdings between the two dates
+      for year in range(start, end + 1):
+         for q in range(1,5):
+            print 'Year ' + str(year) + ' quarter ' + str(q)
             try:
-               sic_inc = sicIndex[fdata[0]] == sic or sic is None
-            except:
-               sic_inc = False # the index might be out of date and missing a company
-
-            if cik_inc and company_inc and form_inc and sic_inc:
-               # if there is a keyphrase search, do that now
-               if keyphrases is not None:
-                  try:
-                     doc = urllib2.urlopen('ftp://ftp.sec.gov/' + fdata[4])
-                     flag = False
-                     for phrase in keyphrases:
-                        if phrase.lower() in doc.read().lower():
-                           flag = True
-                     if not flag:
+               f = urllib2.urlopen('ftp://ftp.sec.gov/edgar/full-index/' + str(year) + '/QTR' 
+                                 + str(q) + '/master.idx')
+            except Exception,e:
+               print str(e)
+               continue
+            lines = f.read().splitlines()[10:]
+            for line in lines:
+               fdata = line.split('|')
+               # Apply the tests from the arguments, storing each in a separate boolean
+               cik_inc = fdata[0] == cik or cik is None
+               company_inc = company is None or fdata[1].lower() == company.lower()
+               form_inc = forms is None or fdata[2] in forms
+               try:
+                  sic_inc = sic is None or sicIndex[fdata[0]] == sic
+               except:
+                  sic_inc = False # the index might be out of date and missing a company
+               if cik_inc and company_inc and form_inc and sic_inc:
+                  # if there is a keyphrase search, do that now
+                  if keyphrases is not None:
+                     try:
+                        doc = urllib2.urlopen('ftp://ftp.sec.gov/' + fdata[4])
+                        flag = False
+                        for phrase in keyphrases:
+                           if phrase.lower() in doc.read().lower():
+                              flag = True
+                        if not flag:
+                           continue
+                     except:
                         continue
-                  except:
-                     continue
-                     # do nothing and just keep going; some files are just missing.
-               results.append({'cik': fdata[0], 'company': fdata[1], 'form': fdata[2],
-                               'date': fdata[3], 'url': 'ftp://ftp.sec.gov/' + fdata[4]})
-   return results
+                        # do nothing and just keep going; some files are just missing.
+                  yield func(fdata)
+   return inner
+
+@edgar
+def query(fdata):
+   return {'cik': fdata[0], 'company': fdata[1], 'form': fdata[2],
+          'date': fdata[3], 'url': 'ftp://ftp.sec.gov/' + fdata[4]}
 
